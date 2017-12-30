cee098e2170ddd89362c58f2ed66b462d8675be0
Michael Lawrence BSc (HONS) <m2lawrence77@outlook.com>
2017-12-30 19:43:03 +0000

    Add files via upload

---
+++ ApplicationState2.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Title="ApplicationState2" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ApplicationState2.aspx.cs" Inherits="ApplicationState.ApplicationState2" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <h1>Application State 2</h1>
+    </div>
+
+    <div class="row">
+        <div class="col-md-4">
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox><asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+
+        </div>      
+    </div>
+
+</asp:Content>
---
+++ ApplicationState2.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ApplicationState
+{
+    public partial class ApplicationState2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (Application["Clicks"] == null)
+                {
+                    Application["Clicks"] = 0;
+                }
+                TextBox1.Text = Application["Clicks"].ToString();
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //here the int variable ClicksCount, stores the Application clicks and increments up 1.
+            int ClicksCount = (int)Application["Clicks"] + 1;
+            //here convert int to string and passes the Application memory to the TextBox1.
+            TextBox1.Text = ClicksCount.ToString();
+            //here the Application memory gets stored back into the int variable ClicksCount.
+            Application["Clicks"] = ClicksCount;
+        }
+    }
+}
